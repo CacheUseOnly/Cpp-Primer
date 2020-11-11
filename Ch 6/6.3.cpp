2fef79600ff9217727879d05635deb9c43aeda20
CacheUseOnly <luoyuxuan.carl@gmail.com>
2020-11-10 23:43:56 -0500

    submit exercise

---
+++ Ch 6/6.3.cpp
@@ -0,0 +1,18 @@
+// Write and test your own version of `fact`
+#include <iostream>
+
+int fact(int n);
+
+int main() {
+    std::cout << "Factorial of 6 is: " << fact(6);
+
+    return 0;
+}
+
+int fact(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n*fact(n-1);
+    }
+}
