b5487656b7e34c5b6feec1c984ff91d43ce75e69
CacheUseOnly <luoyuxuan.carl@gmail.com>
2020-11-24 11:04:13 -0500

    submit exercise

---
+++ Ch 6/6.8.h
@@ -0,0 +1,9 @@
+/**
+ * Write a header file named Chapter6.h that contains
+ * declarations for the functions you wrote 
+ * for the exercises in § 6.1 (p. 205).
+ */
+
+int fact(int n);
+bool calcFacto();
+int absVar(int value);
